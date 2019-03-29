b0a49faee36d8b5593e1a3b9408f72d34ded29a2
yuliana <ysiulil22@gmail.com>
2019-03-29 22:14:32 +0700

    add gitignore

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import {allReducer} from './reducers'
 const store = createStore(allReducer)
-ReactDOM.render(<Router><Provider store={store}><App /></Provider></Router>, document.getElementById('root'));
+ReactDOM.render(<Router><Provider store={store}><MyPage /></Provider></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
